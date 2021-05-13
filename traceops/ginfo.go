54d05528305d8bf1af2c9ed76e53b69b172886c5
Saeed Taheri <staheri@cs.utah.edu>
2021-05-13 01:52:16 -0600

    gtree + export ginfo fields

--- traceops/ginfo.go
+++ traceops/ginfo.go
@@ -21,27 +21,70 @@ var gtypes = []string{"ROOT","MAIN","TRACE","APP","OTHER"}
 
 
 type GInfo struct{
-  gid               uint64
-	parent_id         uint64
-  createStack_id    uint64
-  createStack_frame []*trace.Frame
-  ended             bool
-  gtype             int
-  lastEvent         *trace.Event
+  Gid                  uint64
+	Parent_id            uint64
+  CreateStack_id       uint64
+  CreateStack_frame    []*trace.Frame
+  Ended                bool
+  Gtype                int
+  Events               []*trace.Event
 }
 
 type GoroutineInfo struct{
-	main          *GInfo
-	trace        	*GInfo
-	app           []*GInfo
-  goat          []*GInfo
+	Main          *GInfo
+	Trace        	*GInfo
+	App           []*GInfo
+  Goat          []*GInfo
 }
 
+type GTree struct{
+	Node         *GInfo
+	Children     []*GTree
+}
+
+type GEdge struct{
+	Parent       uint64
+	Child        uint64
+}
+
+func GetGTree(parseResult *trace.ParseResult) (*GTree){
+	gs,gmap,edges := GetGoroutineInfo(parseResult)
+	children := make(map[uint64][]uint64)
+	appids := []uint64{}
+
+	for _,g := range(gs.App){
+		appids = append(appids,g.Gid)
+	}
+
+
+	for _, e := range(edges) {
+		children[e.Parent] = append(children[e.Parent],e.Child)
+	}
+
+	root := &GTree{Node:gs.Main}
+	tovisit := []*GTree{root}
+	for ;len(tovisit)!=0;{
+		cur := tovisit[0]
+		for _,child := range(children[cur.Node.Gid]){
+			if containsUInt64(appids,child){
+				ctree := &GTree{Node:gmap[child]}
+				cur.Children = append(cur.Children,ctree)
+				tovisit = append(tovisit,ctree)
+			}
+		}
+		tovisit = tovisit[1:]
+	}
+	return root
+}
+
+
 // returns goroutine info
-func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint64]*GInfo){
+func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint64]*GInfo, []*GEdge){
 	var ret               GoroutineInfo
 	var notAppGs          []uint64
 	var appGs             []uint64
+	edges := []*GEdge{}
+
 	//var allGs             []uint64
   //var goatGs            []uint64
 //	var gid               uint64
@@ -61,11 +104,12 @@ func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint6
       case "GoCreate":
 				//fmt.Printf(">>> G %v is creating G %d\n",e.G,e.Args[0])
         if _,ok:=gmap[e.Args[0]];!ok{
-          gmap[e.Args[0]] = &GInfo{gid:e.Args[0],
-                                   parent_id:e.G,
-                                   createStack_id:e.StkID,
-                                   createStack_frame: e.Stk,
-																 	 gtype:OTHER}
+          gmap[e.Args[0]] = &GInfo{Gid:e.Args[0],
+                                   Parent_id:e.G,
+                                   CreateStack_id:e.StkID,
+                                   CreateStack_frame: e.Stk,
+																 	 Gtype:OTHER}
+					edges = append(edges,&GEdge{Parent:e.G,Child:e.Args[0]})
 					//fmt.Printf(">>> Add G %v to gmap as OTHER\n",e.Args[0])
 
         } else{
@@ -74,18 +118,18 @@ func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint6
         }
       case "GoEnd":
 				//fmt.Printf(">>> G %v has Ended - update\n",e.G)
-        gi.ended = true
-        gi.lastEvent = e
+        gi.Ended = true
+        gi.Events = append(gi.Events,e)
         gmap[e.G] = gi
         //if gii,ok:=gs[e.G];ok{
         //} else{
           // the child that current e creates has already been existed in the map
-          //panic(fmt.Sprintf("unexplored goroutine %v ended\n",e.G))
+          //panic(fmt.Sprintf("unexplored goroutine %v Ended\n",e.G))
         //}
       default:
 				//fmt.Println("DEFAULT")
 				//fmt.Printf(">>> G %v Default - update last event\n",e.G)
-        gi.lastEvent = e
+        gi.Events = append(gi.Events,e)
         gmap[e.G] = gi
       }
 
@@ -94,18 +138,19 @@ func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint6
 			//fmt.Printf("##### G %v is not in the map\n",e.G)
       if desc.Name == "GoCreate"{
 				//fmt.Printf("##### Add G %v as ROOT\n",e.G)
-        gmap[e.G] = &GInfo{gid:e.G,gtype:ROOT}
+        gmap[e.G] = &GInfo{Gid:e.G,Gtype:ROOT}
 				//fmt.Println("ADDED G0",gmap[e.G].String())
 				//fmt.Printf("##### G %v is creating G %d\n",e.G,e.Args[0])
         if _,ok:=gmap[e.Args[0]];!ok{
-          gmap[e.Args[0]] = &GInfo{gid:e.Args[0],
-                                 parent_id:e.G,
-                                 createStack_id:e.StkID,
-                                 createStack_frame: e.Stk,
-															 	 gtype:OTHER}
+          gmap[e.Args[0]] = &GInfo{Gid:e.Args[0],
+                                 Parent_id:e.G,
+                                 CreateStack_id:e.StkID,
+                                 CreateStack_frame: e.Stk,
+															 	 Gtype:OTHER}
+					edges = append(edges,&GEdge{Parent:e.G,Child:e.Args[0]})
 					//fmt.Printf("##### Add G %v to gmap as OTHER\n",e.Args[0])
         } else{
-          // the child that current e creates has already been existed in the map
+          // the child that current e creates has already been existed in the map, it shouldn't
           panic("child already in the map")
         }
       }else{
@@ -125,7 +170,7 @@ func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint6
 		}// end stack check
 		if len(e.Stk) != 0{
 			frm := e.Stk[0]
-      if strings.HasPrefix(frm.Fn,"runtime/trace.Start") && ret.trace == nil{
+      if strings.HasPrefix(frm.Fn,"runtime/trace.Start") && ret.Trace == nil{
 				if desc.Name != "GoCreate"{
 					panic("trace and main identified in a non-GoCreate event")
 				}
@@ -134,18 +179,18 @@ func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint6
         if !ok{
           panic(fmt.Sprintf("%v key not exist in gmap",e.G))
         }
-        ret.trace = rt
+        ret.Trace = rt
 
-        ret.trace.gtype = TRACE
-        gmap[e.Args[0]] = ret.trace
+        ret.Trace.Gtype = TRACE
+        gmap[e.Args[0]] = ret.Trace
 
-				//fmt.Println(">>>> Main Has Found",gmap[e.Args[0]].parent_id)
-        ret.main, ok = gmap[gmap[e.Args[0]].parent_id]
+				//fmt.Println(">>>> Main Has Found",gmap[e.Args[0]].Parent_id)
+        ret.Main, ok = gmap[gmap[e.Args[0]].Parent_id]
         if !ok{
-          panic(fmt.Sprintf("%v key (parent of trace: %v) not exist in gmap",gmap[e.Args[0]].parent_id,e.G))
+          panic(fmt.Sprintf("%v key (parent of trace: %v) not exist in gmap",gmap[e.Args[0]].Parent_id,e.G))
         }
-        ret.main.gtype = MAIN
-        gmap[gmap[e.Args[0]].parent_id] = ret.main
+        ret.Main.Gtype = MAIN
+        gmap[gmap[e.Args[0]].Parent_id] = ret.Main
       }
 		}
   } // end gmap
@@ -154,13 +199,13 @@ func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint6
 
   for g,ginf := range(gmap){
 		// make sure all gs have last events
-		if ginf.lastEvent == nil{
-			log.Printf("WARNING: G %v has no last event\n",g)
-			fmt.Printf("WARNING: G %v has no last event\n",g)
+		if len(ginf.Events) == 0{
+			log.Printf("WARNING: G %v has no event\n",g)
+			fmt.Printf("WARNING: G %v has no event\n",g)
 		}
 		isApp := true
-		if len(parseResult.Stacks[ginf.createStack_id]) > 0 {
-			for _,frm := range(parseResult.Stacks[ginf.createStack_id]){
+		if len(parseResult.Stacks[ginf.CreateStack_id]) > 0 {
+			for _,frm := range(parseResult.Stacks[ginf.CreateStack_id]){
 				if strings.HasPrefix(frm.Fn,"github.com/staheri/goat/goat.Start"){
 					// this is not app
 					isApp = false
@@ -176,9 +221,9 @@ func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint6
 			//if !containsUInt64(notAppGs,g){
 			//fmt.Printf(">>> g%d is not in NotApps- it is app\n",g)
 			if !containsUInt64(appGs,g){
-				ginf.gtype = APP
+				ginf.Gtype = APP
 				gmap[g] = ginf
-	      ret.app = append(ret.app,ginf)
+	      ret.App = append(ret.App,ginf)
 				appGs = append(appGs,g)
 			}
 		}
@@ -188,7 +233,7 @@ func GetGoroutineInfo(parseResult *trace.ParseResult) (GoroutineInfo , map[uint6
 
 	//StackTable(parseResult.Stacks)
 	//GoroutineTable(gmap)
-	return ret,gmap
+	return ret,gmap,edges
 }
 
 // convert stack trace ([]stack frames) to string
@@ -216,24 +261,29 @@ func ToString(f *trace.Frame) string {
 	return fmt.Sprintf("%s\n\t%s:%d ",f.Fn,f.File,f.Line)
 }
 
+// convert stack frame to string
+func ToKey(f *trace.Frame) string {
+	return fmt.Sprintf("%s:%s:%d",f.File,f.Fn,f.Line)
+}
+
 // returns individual ginfo string
 func (ginf *GInfo) String() string{
-  s := fmt.Sprintf("G: %v\n",ginf.gid)
-  s = s + fmt.Sprintf("Parent: %v\n",ginf.parent_id)
-  s = s + fmt.Sprintf("Ended: %v\n",ginf.ended)
-	s = s + fmt.Sprintf("Type: %v\n",gtypes[ginf.gtype])
-  s = s + fmt.Sprintf("Create StackFrame:\n%v\n",stackToString(ginf.createStack_frame,false))
-	if ginf.lastEvent != nil{
-		s = s + fmt.Sprintf("Last Event: %v\n",trace.EventDescriptions[ginf.lastEvent.Type])
+  s := fmt.Sprintf("G: %v\n",ginf.Gid)
+  s = s + fmt.Sprintf("Parent: %v\n",ginf.Parent_id)
+  s = s + fmt.Sprintf("Ended: %v\n",ginf.Ended)
+	s = s + fmt.Sprintf("Type: %v\n",gtypes[ginf.Gtype])
+  s = s + fmt.Sprintf("Create StackFrame:\n%v\n",stackToString(ginf.CreateStack_frame,false))
+	if len(ginf.Events) != 0{
+		s = s + fmt.Sprintf("Last Event: %v\n",trace.EventDescriptions[ginf.Events[len(ginf.Events)-1].Type])
 	}
   return s
 }
 
 // returns a detail report of execution goroutine structure
 func (ginf *GoroutineInfo) StringDetail() string{
-	s := fmt.Sprintf("Main: \n%v\n",ginf.main.String())
-	s = s +  fmt.Sprintf("Trace: \n%v\n",ginf.trace.String())
-	for _,gi := range(ginf.app){
+	s := fmt.Sprintf("Main: \n%v\n",ginf.Main.String())
+	s = s +  fmt.Sprintf("Trace: \n%v\n",ginf.Trace.String())
+	for _,gi := range(ginf.App){
 		s = s +  fmt.Sprintf("App: \n%v\n---\n",gi.String())
 	}
   // for _,gi := range(ginf.goat){
@@ -244,13 +294,13 @@ func (ginf *GoroutineInfo) StringDetail() string{
 
 // returns a short report of execution goroutine structure
 func (ginf *GoroutineInfo) String() string{
-	s := fmt.Sprintf("Main: %v\n",ginf.main.gid)
-	s = s +  fmt.Sprintf("Trace: %v\n",ginf.trace.gid)
-	for _,gi := range(ginf.app){
-		s = s +  fmt.Sprintf("App: %v\n",gi.gid)
+	s := fmt.Sprintf("Main: %v\n",ginf.Main.Gid)
+	s = s +  fmt.Sprintf("Trace: %v\n",ginf.Trace.Gid)
+	for _,gi := range(ginf.App){
+		s = s +  fmt.Sprintf("App: %v\n",gi.Gid)
 	}
-  for _,gi := range(ginf.goat){
-		s = s +  fmt.Sprintf("GOAT: %v\n",gi.gid)
+  for _,gi := range(ginf.Goat){
+		s = s +  fmt.Sprintf("GOAT: %v\n",gi.Gid)
 	}
 	return s
 }
